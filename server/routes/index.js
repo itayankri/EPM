2ccd5f7d7cdf93f3cf920d0e1efb312548f55176
Ziv Yatziv <zivyatziv@gmail.com>
2019-01-12 14:40:53 +0200

    added accept and decline participation

--- server/routes/index.js
+++ server/routes/index.js
@@ -61,10 +61,10 @@ module.exports = (app) => {
   // TODO: maybe add a $$ money limit system / points limit system / item limit system?
   // other stuff in event
   app.put('/event/:eventId/generateForm') // TODO: Think about generate form
-  app.put('/event/:eventId/claim', participationsController.claim) // claim a participation by a user
-  app.put('/event/:eventId/unclaim', participationsController.unclaim) // unclaim a participation
-  app.put('/event/:eventId/acceptParticipation', participationsController.acceptParticipation) // accept a participation
-  app.put('/event/:eventId/declineParticipation', participationsController.declineParticipation) // decline a participation
+  app.put('/event/:eventId/claim', participationsController.claim); // claim a participation by a user
+  app.put('/event/:eventId/unclaim', participationsController.unclaim); // unclaim a participation
+  app.put('/event/:eventId/acceptParticipation', participationsController.acceptParticipation); // accept a participation
+  app.put('/event/:eventId/declineParticipation', participationsController.declineParticipation); // decline a participation
   app.get('/event/:eventId/campSchedule') // get a schedule for the camp
   app.get('/event/:eventId/chapterSchedule') // get a schedule for the chapter
   app.get('/event/:eventId/contactList', eventsController.contactList); // get the contact list
