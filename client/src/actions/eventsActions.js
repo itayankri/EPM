e4a54131c80a7798b768ab2bffbe5e9d75ec8635
itayankri <itayan116@gmail.com>
2019-02-23 10:52:43 +0200

    Added error snackbar to Events component

--- client/src/actions/eventsActions.js
+++ client/src/actions/eventsActions.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import config from '../config/config';
 
+export const getEvents = () => {
+    return axios.get(`${config.url}/events`);
+};
+
 export const submitEvent = event => {
     return axios.post(`${config.url}/event`, event);
 };
--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -17,6 +17,8 @@ import {
     TableHead,
     TableRow
 } from '@material-ui/core';
+import {getEvents} from "../../actions/eventsActions";
+import ErrorSnackbar from "../common/ErrorSnackbar";
 
 const styles = theme => ({
     root: {
@@ -58,60 +60,102 @@ const rows = [
 ];
 
 class Events extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            events: [],
+            isLoading: true,
+            isErrorSnackbarOpen: false,
+            errorSnackbarMessage: "",
+        }
+    }
+
+    componentWillMount() {
+        getEvents()
+            .then(res => {
+                this.setState({
+                    isLoading: false,
+                    events: res.data
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    isErrorSnackbarOpen: true,
+                    errorSnackbarMessage: `Failed to load Events - ${err}`,
+                })
+            });
+    }
+
+    handleErrorSnackbarClose = event => {
+        this.setState({isErrorSnackbarOpen: false})
+    };
+
+    handleSuccessSnackbarClose = event => {
+        this.setState({isSuccessSnackbarOpen: false})
+    };
+
     render() {
         const {classes} = this.props;
         return (
-            <Grid container spacing={8}>
-                <Grid item md={10}>
-                    <Typography variant="h4" component="h2">
-                        Event Management
-                    </Typography>
-                </Grid>
-                <Grid item md={2}>
-                    <Link
-                        to="/events/create"
-                        className={classes.link}
-                    >
-                        <Button
-                            variant="outlined"
-                            color="primary"
+            <div>
+                <Grid container spacing={8}>
+                    <Grid item md={10}>
+                        <Typography variant="h4" component="h2">
+                            Events Management
+                        </Typography>
+                    </Grid>
+                    <Grid item md={2}>
+                        <Link
+                            to="/events/create"
+                            className={classes.link}
                         >
-                            Create Event
-                        </Button>
-                    </Link>
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                            >
+                                Create Event
+                            </Button>
+                        </Link>
+                    </Grid>
+                    <Grid item md={12}>
+                        <Paper className={classes.root}>
+                            <Table className={classes.table}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Event Name</TableCell>
+                                        <TableCell align="right">Participants</TableCell>
+                                        <TableCell align="right">Date</TableCell>
+                                        <TableCell align="right">Budget</TableCell>
+                                        <TableCell align="right">Rate</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rows.map(row => {
+                                        return (
+                                            <TableRow key={row.id}
+                                                      className={classNames(classes.tableRow, classes.tableRowHover)}>
+                                                <TableCell component="th" scope="row">
+                                                    {row.name}
+                                                </TableCell>
+                                                <TableCell align="right">{row.participants}</TableCell>
+                                                <TableCell align="right">{row.date}</TableCell>
+                                                <TableCell align="right">{row.budget}</TableCell>
+                                                <TableCell align="right">{row.rate}</TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item md={12}>
-                    <Paper className={classes.root}>
-                        <Table className={classes.table}>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Event Name</TableCell>
-                                    <TableCell align="right">Participants</TableCell>
-                                    <TableCell align="right">Date</TableCell>
-                                    <TableCell align="right">Budget</TableCell>
-                                    <TableCell align="right">Rate</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.map(row => {
-                                    return (
-                                        <TableRow key={row.id}
-                                                  className={classNames(classes.tableRow, classes.tableRowHover)}>
-                                            <TableCell component="th" scope="row">
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell align="right">{row.participants}</TableCell>
-                                            <TableCell align="right">{row.date}</TableCell>
-                                            <TableCell align="right">{row.budget}</TableCell>
-                                            <TableCell align="right">{row.rate}</TableCell>
-                                        </TableRow>
-                                    );
-                                })}
-                            </TableBody>
-                        </Table>
-                    </Paper>
-                </Grid>
-            </Grid>
+                <ErrorSnackbar
+                    open={this.state.isErrorSnackbarOpen}
+                    handleClose={this.handleErrorSnackbarClose}
+                    errorMessage={this.state.errorSnackbarMessage}
+                />
+            </div>
         );
     }
 }
