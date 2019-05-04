33708a23f768a16b5e5079db2785cd417b075dcf
Itay <itayan116@gmail.com>
2019-05-04 20:33:16 +0300

    Added menu to comment card

--- client/src/components/Blog/BlogMessageCard.js
+++ client/src/components/Blog/BlogMessageCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,6 +7,11 @@ import Typography from '@material-ui/core/Typography';
 import Fab from "@material-ui/core/Fab";
 import LikeIcon from '@material-ui/icons/ThumbUp';
 import UnlikeIcon from '@material-ui/icons/ThumbDown';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import CardHeader from "@material-ui/core/CardHeader";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const styles = {
     card: {
@@ -24,19 +29,60 @@ const styles = {
     }
 };
 
+const ITEM_HEIGHT = 48;
+
 class BlogMessageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            anchorEl: null
+        }
+    }
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
+        const {anchorEl} = this.state;
+        const open = Boolean(anchorEl);
 
         return (
             <Card className={classes.card}>
+                <CardHeader
+                    action={
+                        <IconButton onClick={this.handleClick}>
+                            <MoreVertIcon/>
+                        </IconButton>
+                    }
+                    title={this.props.title}
+                    subheader={this.props.subtitle}
+                />
+                <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={this.handleClose}
+                    PaperProps={{
+                        style: {
+                            maxHeight: ITEM_HEIGHT * 4.5,
+                            width: 200,
+                        },
+                    }}
+                >
+                    <MenuItem onClick={this.handleClose}>
+                        Edit
+                    </MenuItem>
+                    <MenuItem onClick={this.handleClose}>
+                        Delete
+                    </MenuItem>
+                </Menu>
                 <CardContent>
-                    <Typography variant="h5" component="h2">
-                        {this.props.title}
-                    </Typography>
-                    <Typography className={classes.pos} color="textSecondary">
-                        {this.props.subtitle}
-                    </Typography>
                     <Typography component="p">
                         {this.props.body}
                     </Typography>
