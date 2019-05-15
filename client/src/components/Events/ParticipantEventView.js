b8656251e23b168161d7034ec0b107a147459f38
itayankri <itayan116@gmail.com>
2019-05-15 19:25:33 +0300

    Regular user's event details page started #2

--- client/src/components/Events/ParticipantEventView.js
+++ client/src/components/Events/ParticipantEventView.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {withStyles} from '@material-ui/core/styles';
 import Typography from "@material-ui/core/es/Typography/Typography";
 import Spinner from "./EventSummaryTabView";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
-const styles = theme => ({
+const styles = theme => ({});
 
-});
+const mapStateToProps = state => {
+    return {
+        isUserLoggedIn: state.userDetails.isUserLoggedIn,
+        user: state.userDetails.user,
+    }
+};
 
 class ParticipantEventView extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            status: ""
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props !== prevProps) {
+            if (this.props.event && this.props.user) {
+                for (let participation of this.props.event.participations) {
+                    if (participation.User.id === this.props.user.id) {
+                        this.setState({status: participation.status});
+                    }
+                }
+            }
+        }
     }
 
     render() {
@@ -36,9 +57,9 @@ class ParticipantEventView extends React.Component {
                         style={{
                             marginLeft: 10,
                             color: 'red'
-                    }}
+                        }}
                     >
-                        Participation Status: UNCLAIMED
+                        Participation Status: {this.state.status}
                     </Button>
                 </Typography>
                 {/*<Typography variant="subheading">Participation Status: UNCLAIMED</Typography>*/}
@@ -75,10 +96,11 @@ class ParticipantEventView extends React.Component {
                     </Button>
                 </Grid>
                 <br/>
-                <Typography>Some descriptions text that describes the descriptions of the event that described in this description.</Typography>
+                <Typography>Some descriptions text that describes the descriptions of the event that described in this
+                    description.</Typography>
             </div>
         );
     }
 }
 
-export default withStyles(styles)(ParticipantEventView);
+export default connect(mapStateToProps)(withStyles(styles)(ParticipantEventView));
