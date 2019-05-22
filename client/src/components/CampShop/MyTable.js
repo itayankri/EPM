8ca7af1e2f8ce82d711da2a98a08169bee6910f7
Ziv Yatziv <zivyatziv@gmail.com>
2019-05-23 01:17:42 +0300

    Made CampShop more modular

---
+++ client/src/components/CampShop/MyTable.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import classNames from 'classnames';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
+
+function MyTable(props) {
+    const { coloredColumns, columnsColor, otherColumns, data, classes, values, innerCellMethod } = props;
+
+    return (
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        {coloredColumns.map(col => {
+                            return (
+                                <TableCell align="center" style={{ backgroundColor: columnsColor }}>
+                                {col}
+                                </TableCell>
+                            )
+                        })
+                        }
+                        {Object.keys(otherColumns).map(row => {
+                            return (
+                                <TableCell align="center">
+                                    {row} ({otherColumns[row]})
+                                </TableCell>
+                            );
+                        })}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map(row => {
+                        return (
+                            <TableRow
+                                key={row.id}
+                                className={classNames(classes.tableRow, classes.tableRowHover)}
+                            >
+                                <TableCell align="center">{row.User.country}</TableCell>
+                                <TableCell align="center">{row.User.firstName} {row.User.lastName}</TableCell>
+                                {Object.keys(otherColumns).map(item => {
+                                    return (
+                                        <TableCell align="center">
+                                            {innerCellMethod(row, item)}
+                                        </TableCell>
+                                    );
+                                })}
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+}
+
+MyTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MyTable);
